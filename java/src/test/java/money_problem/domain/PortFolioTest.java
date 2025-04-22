14d1f7d461ac8432062a5140fed7a4e907cdca94
Aymoreira <moreira.aymeric@gmail.com>
2025-04-22 17:57:29 +0200

    Portfolio fait avec evaluate pour differente currency

--- java/src/test/java/money_problem/domain/PortFolioTest.java
+++ java/src/test/java/money_problem/domain/PortFolioTest.java
@@ -2,23 +2,69 @@ package money_problem.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 class PortFolio{
-    void add(int amount, Currency currency) {
+
+    private double amount;
+    private HashMap<Currency, Double> amounts = new HashMap<>();
+
+    void add(double amount, Currency currency) {
         // Implementation for adding money to the portfolio
+        this.amount += amount;
+        if(this.amounts.get(currency) == null){
+            this.amounts.put(currency, amount);
+        }else{
+            this.amounts.put(currency, this.amounts.get(currency) + amount);
+        }
+        
+
     }
 
-    int evaluate(Bank bank) {
+    double evaluate(Bank bank, Currency currency){
         // Implementation for evaluating the portfolio
-        return 5;
+
+        double totalAmountConverted = 0;
+
+        for (Map.Entry<Currency, Double> entry : this.amounts.entrySet()) {
+            try {
+                
+                System.out.println(entry.getValue().toString() + " " +  entry.getKey().toString() + " " + currency.toString());
+                double newAmount = bank.convert(entry.getValue(), entry.getKey(), currency);
+                System.out.println(newAmount);
+                totalAmountConverted += newAmount;
+            } catch (MissingExchangeRateException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return totalAmountConverted;
     }
 }
 
 public class PortFolioTest {
-    
+
     @Test
-    void shouldAddInUsd() {
+    void shouldAddOneValueInSameCurency() {
+        
+        // Arange
+        Bank bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2);
+        
+        PortFolio portfolio = new PortFolio();
+        portfolio.add(5, Currency.USD);
+        
+        // Act
+        double evaluation = portfolio.evaluate(bank, Currency.USD);
+        
+        //Assert
+        assertEquals(evaluation, 5);
+    }
+
+    @Test
+    void shouldAddInSameCurency() {
         
         // Arange
         Bank bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2);
@@ -28,9 +74,24 @@ public class PortFolioTest {
         portfolio.add(10, Currency.USD);
         
         // Act
-        int evaluation = portfolio.evaluate(bank);
+        double evaluation = portfolio.evaluate(bank, Currency.USD);
         
         //Assert
-        assertEquals(evaluation, 5);
+        assertEquals(evaluation, 15);
+    }
+
+    @Test
+    // add 5 usd + 10 EUR qhould be 17 USD
+    void shouldAddInUsd() {
+        PortFolio portfolio = new PortFolio();
+        portfolio.add(5, Currency.USD);
+        portfolio.add(10, Currency.EUR);
+
+        Bank bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2);
+
+
+        double evaluation = portfolio.evaluate(bank, Currency.USD);
+
+        assertEquals(evaluation, 17);       
     }
 }

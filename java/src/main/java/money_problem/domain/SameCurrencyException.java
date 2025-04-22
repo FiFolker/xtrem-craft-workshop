c3b6c123e6048f6b279a4c42482f686aac6cdf50
Le Merle Elouan <elouan.le-merle@etu.u-bordeaux.fr>
2025-04-22 14:53:01 +0200

    suppression du ficheir same Currency Exception

--- java/src/main/java/money_problem/domain/SameCurrencyException.java
+++
@@ -1,7 +0,0 @@
-package money_problem.domain;
-
-public class SameCurrencyException extends Exception {
-    public SameCurrencyException(Currency start, Currency end) {
-        super(String.format("Impossible de convertir deux monnaies identiques %s->%s", start, end));
-    }
-}

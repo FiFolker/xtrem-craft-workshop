985fd3d1329c27f02821a7425515c3d34b64edbe
Kamila <kamimdlll@gmail.com>
2025-06-17 17:33:31 +0200

    bankbuilder

--- java/src/test/java/money_problem/domain/PortFolioTest.java
+++ java/src/test/java/money_problem/domain/PortFolioTest.java
@@ -6,6 +6,37 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
+class BankBuilder{
+    private Currency pivotCurrency;
+    private final Map<Currency, Double> exchangeRates = new HashMap<>();
+
+    public static BankBuilder abank() {
+        return new BankBuilder();
+    }
+
+    public BankBuilder withPivot(Currency pivotCurrency) {
+        this.pivotCurrency = pivotCurrency;
+        return this;
+    }
+
+    public BankBuilder withExchangeRate(double rate, Currency targetCurrency) {
+        this.exchangeRates.put(targetCurrency, rate);
+        return this;
+    }
+
+    public build(){
+        Currency currency = this.exchangeRates.keys()[0];
+        Bank bank = Bank.withExchangeRate(this.pivotCurrency, currency, this.exchangeRates.get(currency));
+        for (Map.Entry<Currency, Double> entry : exchangeRates.entrySet()) {
+            if(entry.getKey() != currency){
+                bank.addExchangeRate(this.pivotCurrency, entry.getKey(), entry.getValue());
+            }
+        }
+        return bank;
+    }
+}
+
+
 public class PortFolioTest {
 
     @Test
@@ -13,15 +44,19 @@ public class PortFolioTest {
         
         // Arange
         Bank bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2);
+
+        Bank bank2 = BankBuilder.abank().withPivot(Currency.EUR).withExchangeRate(1.2, Currency.USD).build();
         
         PortFolio portfolio = new PortFolio();
         portfolio.add(5, Currency.USD);
         
         // Act
         Money evaluation = portfolio.evaluate(bank, Currency.USD);
+        Money evaluation2 = portfolio.evaluate(bank2, Currency.USD)
         
         //Assert
         assertEquals(evaluation, new Money(5, USD));
+        assertEquals(evaluation2, new Money(5, USD));
     }
 
     @Test
